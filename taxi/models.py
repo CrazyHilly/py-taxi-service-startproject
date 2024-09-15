26841194b7867d358a49e76571500a0b5f1d9c5f
Yuliia Dudych <juliadudych@gmail.com>
2024-09-15 20:56:19 +0300

    Create Manufacturer model

---
+++ taxi/models.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    country = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
