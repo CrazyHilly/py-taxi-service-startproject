48cf275f96e0c0d90db2d97f07b23ae8a34fbea7
Yuliia Dudych <juliadudych@gmail.com>
2024-09-15 20:56:49 +0300

    Create Driver model

--- taxi/models.py
+++ taxi/models.py
@@ -9,3 +9,10 @@ class Manufacturer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=10, unique=True)
+
+    class Meta:
+        verbose_name = "Driver"
